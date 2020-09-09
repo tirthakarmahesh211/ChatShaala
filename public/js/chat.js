f1d571b905b6e99a199649e227debb31ac47cbd7
tirthakarmahesh211 <tirthakarmahesh211@gmail.com>
2020-09-09 15:46:20 +0530

    duplicate code removed

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,7 +21,6 @@ function preloadFunc(){
     document.getElementById("saveDelta").style.display = "block";
     document.getElementById("upload_files").style.display = "block";
     document.getElementById("right_panel_msg").style.display = "none";
-    document.getElementById("right_panel_msg").style.display = "none";
   }
   else{
     document.getElementById("plus_btn").style.display = "none";
@@ -30,7 +29,6 @@ function preloadFunc(){
     document.getElementById("login_link").style.display = "block";
     document.getElementById("saveDelta").style.display = "none";
     document.getElementById("upload_files").style.display = "none";
-    document.getElementById("right_panel_msg").style.display = "none";
     document.getElementById("right_panel_msg").style.display = "block";
     document.getElementById("replyMessage").placeholder = "Please Login to join the conversation"
   } 
